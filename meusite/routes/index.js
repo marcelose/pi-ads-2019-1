a5e328ac412ac90dabe34fc3cd7d4b7513496a0e
José Yoshiriro <jyoshiriro@gmail.com>
2019-05-12 15:30:25 -0300

    teste rx

--- meusite/routes/index.js
+++ meusite/routes/index.js
@@ -10,5 +10,8 @@ router.get('/r', function(req, res, next) {
   res.send([{v:0, e:2, d:3}, {v:0, e:2, d:3}, {v:0, e:2, d:3}]); 
 });
 
+router.get('/rx', function(req, res, next) {
+  res.status(403).send({x:0, www:2, ehehe:3}); 
+});
 
 module.exports = router;
